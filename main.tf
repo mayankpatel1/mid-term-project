4d4720a0836aa30c80dd903f95c11f11bec19e08
mayankpatel1 <146357011+mayankpatel1@users.noreply.github.com>
2023-10-20 12:30:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,32 @@ module "s3bucket" {
   path = var.path
   style_path = var.style_path
 }
+module "aws_instance" "ec2_instance" {
+  source = "./ec2"
+  aws_instance_name = var.instance.name
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  security_groups = [var.security_group]
+
+  tags = {
+    Name = var.instance_name
+  }
+}    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "vpc" {
+    source = "./vpc"
+    tags = var.tags
+    igw_tags = var.igw_tags
+    public_subnet_tags = var.public_subnet_tags
+    private_subnet_tags = var.private_subnet_tags
+    public_route_table_tags = var.public_route_table_tags
+    nat_tags = var.nat_tags
+    private_route_table_tags = var.private_route_table_tags
+    
+  }
+  
