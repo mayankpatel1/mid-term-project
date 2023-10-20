98a9c2b1a96ffe35c6e9332a96ecab44ce32d793
mayankpatel1 <146357011+mayankpatel1@users.noreply.github.com>
2023-10-20 12:47:33 +0100

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,1 +1,29 @@
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket for hosting the static website"
+  type        = string
+}
+
+variable "s3_bucket_acl" {
+  description = "The access control list (ACL) for the S3 bucket"
+  type        = string
+  default     = "public-read"  #
+}
+
+variable "s3_index_document" {
+  description = "The index document for the static website"
+  type        = string
+  default     = "index.html"  #
+}
+
+variable "s3_error_document" {
+  description = "The error document for the static website"
+  type        = string
+  default     = "error.html"  
+}
+
+variable "kms_key_id" {
+  description = "The ID of the KMS key for server-side encryption"
+  type        = string
+  
+}
 
